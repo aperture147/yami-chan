e8b6a1d69f282c554d5fe81b644dbdb6be2597c6
Aperture <aperture147@gmail.com>
2020-10-27 16:42:59 +0700

    Add set presence

--- bot.ts
+++ bot.ts
@@ -5,6 +5,14 @@ const client = new Client();
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setPresence({
+        activity: {
+            name: 'Ylang Ylang EP (FKJ)',
+            url: 'https://www.youtube.com/watch?v=pfU0QORkRpY',
+            type: 'STREAMING'
+        },
+        status: 'online'
+    }).then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
 });
 
 client.on('message', msg => {
