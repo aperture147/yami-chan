a358d0dce9b280a88de2ae3a52e41347d8f23b9a
Aperture <aperture147@gmail.com>
2020-10-28 20:45:43 +0700

    Correct ping

--- command/ping.ts
+++ command/ping.ts
@@ -15,15 +15,15 @@ export const help: HelpInfo = {
 }
 
 export function ping(msg: Message): void {
-    const now = Date.now()
-    const discordLatency = Date.now() - msg.createdTimestamp
+    const now: number = Date.now()
+    const discordLatency: number = Math.abs(Date.now() - msg.createdTimestamp)
     client.db("yami").admin().ping().then(() => {
         const embed = new MessageEmbed()
             .setTitle("Pong!")
             .setColor(0xff0000)
             .setDescription(
                 `Discord: \`${discordLatency}ms\`\n` +
-                `Database: \`${Date.now() - now}ms\``)
+                `Database: \`${Math.abs(Date.now() - now)}ms\``)
         msg.channel.send(embed).then(() => msg.delete())
     }).catch(err => {
         const embed = new MessageEmbed()
